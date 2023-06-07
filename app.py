c2f2e80adba6d7c53741dcaa103fbda31c040fdd
Geonobins <geonobins88@gmail.com>
2023-06-07 05:40:02 +0530

    Added new feature to provide study materials

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template , request
 
 import marks as m
+import materials as ma
 
 app = Flask(__name__)
 
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/predict",methods=["GET","POST"])
 def marks():
     marks_pred=0
     if request.method=="POST":
@@ -13,15 +14,16 @@ def marks():
         marks_pred= m.marks_prediction(hrs)
     return render_template("index.html",my_marks = marks_pred)
 
-"""
-@app.route("/sub", methods = ['POST'])
-def submit():
-    if request.method == "POST":
-        name = request.form["username"]
+@app.route("/materials", methods = ["GET", "POST"])
+def materials():
+    material={}
+    if request.method=="POST":
+        subject = request.form["subject"]
+        material= ma.scrape_youtube_videos(subject)
+    links_list = [{'title': title, 'url': url} for title, url in material.items()]
+    return render_template('index.html', links=links_list)
 
-    return render_template("sub.html",n=name)  
-
-"""    
+  
   
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ materials.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+def scrape_youtube_videos(search_query):
+    # Concatenate the search query with "ktu"
+    search_string = search_query + " ktu"
+    
+    # Define the YouTube Data API search URL
+    url = f"https://www.googleapis.com/youtube/v3/search?key=AIzaSyCMk2A33GEN0uYtCxHU4cCroN31wGjTu0M&q={search_string}&part=snippet&type=video&maxResults=5"
+    
+    # Make a GET request to the API
+    response = requests.get(url)
+    
+    # Parse the JSON response
+    data = json.loads(response.text)
+    
+    # Extract the video URLs and titles from the response
+    video_data = {}
+    for item in data["items"]:
+        video_id = item["id"]["videoId"]
+        video_url = f"https://www.youtube.com/watch?v={video_id}"
+        video_title = item["snippet"]["title"]
+        video_data[video_title] = video_url
+    
+    # Return the video data (titles and URLs)
+    return video_data
+
