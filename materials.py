42034e22b1584100c930b419c137433a093355be
Geonobins <geonobins88@gmail.com>
2023-06-07 05:54:35 +0530

    security changes

--- materials.py
+++ materials.py
@@ -6,7 +6,7 @@ def scrape_youtube_videos(search_query):
     search_string = search_query + " ktu"
     
     # Define the YouTube Data API search URL
-    url = f"https://www.googleapis.com/youtube/v3/search?key=AIzaSyCMk2A33GEN0uYtCxHU4cCroN31wGjTu0M&q={search_string}&part=snippet&type=video&maxResults=5"
+    url = f"https://www.googleapis.com/youtube/v3/search?key=YourAPIKey&q={search_string}&part=snippet&type=video&maxResults=5"
     
     # Make a GET request to the API
     response = requests.get(url)
