2c0acd0a8f17cfc1b4af4dd680a9bf6a3b1efae9
Muhammed-Ajnas <120674741+Muhammed-Ajnas@users.noreply.github.com>
2023-06-16 19:15:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template , request
-
+from flask import Flask, render_template , request , redirect, session, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
+from flask import flash
 import marks as m
 import materials as ma
 
@@ -7,9 +10,64 @@ import datetime
 import pickle
 
 app = Flask(__name__)
+app.secret_key = "afaafaefaagaegeg"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    
+@app.route("/")
+def home():
+    return render_template("home.html")
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            return redirect(url_for("marks"))
+
+        flash("Invalid email or password. Please try again.")
+        return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        email = request.form.get("email")
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash("Username already exists", "error")
+            return redirect(url_for("signup"))
+
+        new_user = User(username=username, password=generate_password_hash(password), email=email)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("Sign up successful! Please login.", "success")
+        return redirect(url_for("login"))
+
+    return render_template("signup.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("user_id", None)
+    return redirect(url_for("home"))
+
+@app.route("/index",methods=["GET","POST"])
 def marks():
     marks_pred=0
     study_hour_list={}
